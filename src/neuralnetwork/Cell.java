8828d09427ee7c25317a396744039fd64ba635c8
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-10-31 11:38:45 +0800

    finished up to check exmaple (size of input and output)

--- src/neuralnetwork/Cell.java
+++ src/neuralnetwork/Cell.java
@@ -2,32 +2,47 @@ package neuralnetwork;
 
 import java.util.Vector;
 
+import com.mysql.jdbc.Util;
+
 import myutils.Utils;
 
 public class Cell {
+	private static int NewID = 0;
 	public int id;
 	public int layerid;
 	public Vector<Connection> connections;
 	public double delta;
+	public double value;
 
+	/** Static methods **/
+	public static int getID() {
+		return NewID++;
+	}
+
+	/** instance methods **/
 	public Cell(int id, int layerid) {
 		this.id = id;
 		this.layerid = layerid;
 		connections = new Vector<Connection>();
 		delta = Utils.random.nextDouble();
+		value = Utils.random.nextDouble();
 	}
 
-	public Cell(int id, int layerid, double delta) {
+	public Cell(int id, int layerid, double delta, double value) {
 		this.id = id;
 		this.layerid = layerid;
 		connections = new Vector<Connection>();
 		this.delta = delta;
+		this.value = value;
 	}
 
-	public Cell(int id, int layerid, Vector<Connection> connections, double delta) {
+	public Cell(int id, int layerid, Vector<Connection> connections, double delta,
+			double value) {
 		this.id = id;
 		this.layerid = layerid;
 		this.connections = connections;
 		this.delta = delta;
+		this.value = value;
 	}
+
 }
--- src/neuralnetwork/NeuralNetwork.java
+++ src/neuralnetwork/NeuralNetwork.java
@@ -7,33 +7,29 @@ import myutils.SqlServerInfo;
 public class NeuralNetwork {
 	private SqlServerInfo sqlServerInfo;
 	private String mode;
-	private int[] layers;
+	private int[] layers_ints;
 	private Vector<Cell> cells;
+	private Vector<Layer> layers;
 
-	public NeuralNetwork(SqlServerInfo sqlServerInfo, String mode, int[] layers) {
+	public NeuralNetwork(SqlServerInfo sqlServerInfo, String mode, int[] layers_ints) {
 		if (mode != "BackNN") {
 			System.out.println("[" + "mode" + "] Mode not supported");
 			System.exit(1);
 		}
 		this.sqlServerInfo = (SqlServerInfo) sqlServerInfo.clone();
 		this.mode = mode;
-		this.layers = layers.clone();
+		this.layers_ints = layers_ints.clone();
 	}
 
 	/** java staff **/
 	public void create() {
-		createCells();
-		createConnections();
+		createLayers();
 	}
 
-	private void createCells() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void createConnections() {
-		// TODO Auto-generated method stub
-
+	private void createLayers() {
+		layers = new Vector<Layer>();
+		for (int iLayer = 0; iLayer < layers_ints.length; iLayer++)
+			layers.add(new Layer(iLayer, layers_ints[iLayer]));
 	}
 
 	/** sql staff **/
@@ -44,4 +40,36 @@ public class NeuralNetwork {
 	private void loadFromDB() {
 		// TODO Auto-generated method stub
 	}
+
+	/** neural network staff **/
+	public class Example {
+		public Vector<Double> input;
+		public Vector<Double> output;
+
+		public Example(Vector<Double> input, Vector<Double> output) {
+			this.input = input;
+			this.output = output;
+		}
+
+		public void checkInput(int n) {
+			while (input.size() < n)
+				input.add(0d);
+		}
+
+		public void checkOutput(int n) {
+			while (output.size() < n)
+				output.add(0d);
+		}
+	}
+
+	public void learn(Vector<Example> examples) {
+		for (Example example : examples)
+			learn(example);
+	}
+
+	private void learn(Example example) {
+		example.checkInput(layers.get(0).cells.size());
+		example.checkOutput(layers.get(layers.size()).cells.size());
+	}
+
 }
