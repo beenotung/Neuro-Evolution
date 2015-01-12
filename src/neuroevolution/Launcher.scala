1ab28afe4db27ed5e7b47a2cf809c1d7ebbb7564
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-01-12 21:44:07 +0800

    NeuroEvolution: to build run(input) part

--- src/neuroevolution/Launcher.scala
+++ src/neuroevolution/Launcher.scala
@@ -3,6 +3,14 @@ package neuroevolution
 /**
  * Created by beenotung on 1/11/15.
  */
-class Launcher {
+object Launcher extends App {
 
+  def start: Unit = {
+    test
+  }
+
+  def test: Unit = {
+    println("start")
+    println("end")
+  }
 }
