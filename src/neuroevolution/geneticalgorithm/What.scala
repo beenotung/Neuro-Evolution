627d28afdbb4e3669fbccca54d30d8d9483ef778
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-03-25 22:24:38 +0800

    to build fusion (use GA and neural network)

--- src/neuroevolution/geneticalgorithm/What.scala
+++
@@ -1,8 +0,0 @@
-package neuroevolution.geneticalgorithm
-
-/**
- * Created by 13058536A on 2/17/2015.
- */
-class What(val rawCode: Array[Boolean], val N_BIT_WEIGHT: Int, val N_BIT_BIAS: Int) {
-  val genes: Array[What] = new Array[What](32)
-}
