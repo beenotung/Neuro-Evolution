5b50aeebec04525c84ece01b1c2c0b0e161d414d
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-04-01 08:57:35 +0800

    performance update

--- src/neuroevolution/NeuroEvolution.scala
+++ src/neuroevolution/NeuroEvolution.scala
@@ -5,6 +5,8 @@ import neuroevolution.geneticalgorithm.GA.ProblemType
 import neuroevolution.geneticalgorithm.GA.ProblemType.ProblemType
 import neuroevolution.neuralnetwork.Perceptron
 
+import scala.collection.parallel.mutable.ParArray
+
 /**
  * Created by beenotung on 3/25/15.
  */
@@ -22,8 +24,8 @@ class NeuroEvolution(n_Bit_Weight: Int, n_Bit_Bias: Int, numberOfNodes: Array[In
   val converter: Converter = new Converter(N_BIT_WEIGHT = n_Bit_Weight, N_BIT_BIAS = n_Bit_Bias, numberOfNodes, BIT_SIZE = bitSize)
   val ga: GA = new GA(POP_SIZE = popSize, BIT_SIZE = bitSize, P_SELECTION = pSelection, P_MUTATION = pMutation, A_MUTATION = aMutation, EVAL_FITNESS_FUNCTION = evalFitness_function, PROBLEM_TYPE = problemType)
 
-  def evalFitness_function(rawCode: Array[Boolean]): Double = {
-    val perceptron: Perceptron = converter.decode(rawCode)
+  def evalFitness_function(rawCode: ParArray[Boolean]): Double = {
+    val perceptron: Perceptron = converter.decode(rawCode.toArray)
     eval_perceptron_function(perceptron.run(get_perceptron_input))
   }
 
