28332613b689413df87463d7256fd6e1e1ab087f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-04-01 08:33:38 +0800

    enhanced GA into parallel computing

--- src/neuroevolution/geneticalgorithm/GA.scala
+++ src/neuroevolution/geneticalgorithm/GA.scala
@@ -1,9 +1,13 @@
 package neuroevolution.geneticalgorithm
 
 
+import java.util.concurrent.Semaphore
+
 import neuroevolution.geneticalgorithm.GA.ProblemType
 import neuroevolution.geneticalgorithm.GA.ProblemType.ProblemType
 import neuroevolution.utils.Utils
+
+import scala.collection.parallel.mutable.ParArray
 
 /**
  * Created by beenotung on 1/30/15.
@@ -14,32 +18,44 @@ This type of genetic algorithm find rawCode rawCode for MAXIMUM fitness
 */
 
 object GA {
+
   object ProblemType extends Enumeration {
     type ProblemType = super.Value
     val Maximize, Minimize = Value
   }
+
 }
 
-class GA(POP_SIZE: Int, BIT_SIZE: Int, P_SELECTION: Double,
+class GA(POP_SIZE: Int, var BIT_SIZE: Int, P_SELECTION: Double,
          P_MUTATION: Double, A_MUTATION: Double,
-         EVAL_FITNESS_FUNCTION: Array[Boolean] => Double,
+         EVAL_FITNESS_FUNCTION: ParArray[Boolean] => Double,
          PROBLEM_TYPE: ProblemType = ProblemType.Maximize,
          var LOOP_INTERVAL: Long = 100)
   extends Thread {
-  var genes: Array[Gene] = new Array[Gene](POP_SIZE)
+  val loopSemaphore: Semaphore = new Semaphore(1)
+  var genes: ParArray[Gene] = ParArray.fill[Gene](POP_SIZE)(new Gene(BIT_SIZE, A_MUTATION, EVAL_FITNESS_FUNCTION))
+
+  def resize(newBitSize: Int) = {
+    loopSemaphore.tryAcquire()
+    BIT_SIZE = newBitSize
+    for (gene <- genes)
+      gene.resize(newBitSize)
+    loopSemaphore.release()
+  }
 
   def setup = {
-    for (i <- genes.indices) {
-      genes(i) = new Gene(BIT_SIZE, A_MUTATION, EVAL_FITNESS_FUNCTION)
-    }
     eval
+    sort
   }
 
   def loop = {
+    loopSemaphore.tryAcquire()
     select
     crossover
     mutation
     eval
+    sort
+    loopSemaphore.release()
   }
 
   def eval = {
@@ -47,60 +63,53 @@ class GA(POP_SIZE: Int, BIT_SIZE: Int, P_SELECTION: Double,
     evalDiversity
     for (gene <- genes)
       gene.eval(0.5d)
-    sort
   }
 
   def evalFitness = {
-    for (gene <- genes)
-      gene.evalFitness
-    evalDiversity
+    genes.foreach(gene => gene.evalFitness)
   }
 
   def evalDiversity = {
     //TODO diversity
-    val centroid: Array[Double] = new Array[Double](BIT_SIZE)
+    val centroid: ParArray[Double] = ParArray.fill[Double](BIT_SIZE)(0)
 
-    for (bit <- centroid.indices)
-      centroid(bit) = 0
+    genes.foreach(gene => Range(0, gene.rawCode.length).par.foreach(i => if (gene.rawCode(i)) centroid(i) += 1))
 
-    for (gene <- genes)
-      for (bit <- centroid.indices)
-        if (gene.rawCode(bit))
-          centroid(bit) += 1
+    Range(0, centroid.length).par.foreach(i => centroid(i) /= POP_SIZE)
 
-    for (bit <- centroid.indices)
-      centroid(bit) /= POP_SIZE
+    genes.foreach(gene => gene.evalDiversity(centroid))
+
 
     for (gene <- genes)
       gene.evalDiversity(centroid)
   }
 
   def sort = {
-    genes = genes.sorted
-    if (PROBLEM_TYPE.equals(GA.ProblemType.Maximize)) genes = genes.reverse
+    var sortedGenes: Array[Gene] = genes.toArray[Gene].sorted
+    if (PROBLEM_TYPE.equals(GA.ProblemType.Maximize)) sortedGenes = sortedGenes.reverse
+    genes = sortedGenes.toVector.toParArray
   }
 
   def select = {
-    genes = genes.sorted
     val popTotal: Double = POP_SIZE * 1D
-    for (i <- genes.indices)
-      genes(i).selected = (i / popTotal) <= P_SELECTION
+    Range(0, genes.length).par.foreach(i => genes(i).selected = (i / popTotal) <= P_SELECTION)
   }
 
   def crossover = {
-    var p1, p2 = 0
-    for (gene <- genes)
-      if (!gene.selected) {
-        do{p1 = Utils.random.nextInt(POP_SIZE)}while(genes(p1).selected)
-        do{p2 = Utils.random.nextInt(POP_SIZE)}while(genes(p1).selected|| p1.equals(p2))
-        gene.crossover(genes(p1), genes(p2))
-      }
+    genes.foreach(gene => if (!gene.selected) {
+      var p1, p2 = 0
+      do {
+        p1 = Utils.random.nextInt(POP_SIZE)
+      } while (!genes(p1).selected)
+      do {
+        p2 = Utils.random.nextInt(POP_SIZE)
+      } while (!genes(p2).selected || p1.equals(p2))
+      gene.crossover(genes(p1), genes(p2))
+    })
   }
 
   def mutation = {
-    for (gene <- genes)
-      if (Utils.random.nextDouble() < P_MUTATION)
-        gene.mutation
+    genes.foreach(gene => if (Utils.random.nextDouble() < P_MUTATION) gene.mutation)
   }
 
   override def run = {
@@ -110,10 +119,12 @@ class GA(POP_SIZE: Int, BIT_SIZE: Int, P_SELECTION: Double,
       Thread.sleep(LOOP_INTERVAL)
     }
   }
-  def getBestGene:Gene={
-    genes(0)
-  }
-  def getBestRawCode:Array[Boolean]={
+
+  def getBestRawCode: ParArray[Boolean] = {
     getBestGene.rawCode
+  }
+
+  def getBestGene: Gene = {
+    genes.head
   }
 }
--- src/neuroevolution/geneticalgorithm/Gene.scala
+++ src/neuroevolution/geneticalgorithm/Gene.scala
@@ -2,23 +2,30 @@ package neuroevolution.geneticalgorithm
 
 import neuroevolution.utils.{DataTypes, Utils}
 
+import scala.collection.parallel.mutable.ParArray
+
 
 /**
  * Created by beenotung on 3/17/15.
  */
-class Gene(BIT_SIZE: Int, A_MUTATION: Double = 0.1d, evalFitness_function: (Array[Boolean]) => Double)
+class Gene(BIT_SIZE: Int, A_MUTATION: Double = 0.1d, evalFitness_function: (ParArray[Boolean]) => Double)
   extends Comparable[Gene] {
-  val rawCode: Array[Boolean] = new Array[Boolean](BIT_SIZE)
+  var rawCode: ParArray[Boolean] = new ParArray[Boolean](BIT_SIZE)
   var fitness: Double = 0D
   var diversity: Double = 1D
   var preference: Double = 0.5D
   var selected: Boolean = true
 
-
-  def eval(fitnessWeight: Double):Double = {
-    eval(fitnessWeight,1-fitnessWeight)
+  def resize(newBitSize: Int) = {
+    val newRawCode: ParArray[Boolean] = ParArray.tabulate[Boolean](newBitSize) { (i) => if (i < rawCode.length) rawCode(i) else Utils.random.nextBoolean() }
+    rawCode = newRawCode
   }
-  private def eval(fitnessWeight: Double, diversityWeight: Double) :Double= {
+
+  def eval(fitnessWeight: Double): Double = {
+    eval(fitnessWeight, 1 - fitnessWeight)
+  }
+
+  private def eval(fitnessWeight: Double, diversityWeight: Double): Double = {
     preference = fitness * fitnessWeight + diversity * diversityWeight
     preference
   }
@@ -27,11 +34,10 @@ class Gene(BIT_SIZE: Int, A_MUTATION: Double = 0.1d, evalFitness_function: (Arra
     fitness = evalFitness_function(rawCode)
   }
 
-  def evalDiversity(centroid: Array[Double]) = {
+  def evalDiversity(centroid: ParArray[Double]) = {
     diversity = 0
-    for (bit <- rawCode.indices)
-    //diversity+=Math.abs(rawCode(x)-centroid(x))
-      diversity += Math.pow(DataTypes.-(rawCode(bit), centroid(bit)), 2)
+    for (i <- Range(0, rawCode.length))
+      diversity += Math.pow(DataTypes.-(rawCode(i), centroid(i)), 2)
   }
 
   override def compareTo(o: Gene): Int = {
@@ -39,17 +45,17 @@ class Gene(BIT_SIZE: Int, A_MUTATION: Double = 0.1d, evalFitness_function: (Arra
   }
 
   def crossover(p1: Gene, p2: Gene): Unit = {
-    for (i <- rawCode.indices)
+    Range(0, rawCode.length).par.foreach(i =>
       if (Utils.random.nextBoolean())
         rawCode(i) = p1.rawCode(i)
       else
-        rawCode(i) = p2.rawCode(i)
+        rawCode(i) = p2.rawCode(i))
   }
 
   def mutation: Unit = {
-    for (i <- rawCode.indices)
-      if (Utils.random.nextDouble() < A_MUTATION)
-        rawCode(i) ^= true
+    Range(0, rawCode.length).par.foreach(i =>
+      if(Utils.random.nextDouble()<A_MUTATION) rawCode(i)^=true
+    )
   }
 }
 
